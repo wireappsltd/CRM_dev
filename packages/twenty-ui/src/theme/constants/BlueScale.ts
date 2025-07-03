e6236d2786b4cbcfe4e1792aabf49ef074e1d9d8
I.A.B.P Rathnayake <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-07-03 20:50:04 +0530

    Defined colors

---
+++ packages/twenty-ui/src/theme/constants/BlueScale.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @nx/workspace-no-hardcoded-colors */
+export const BLUE_SCALE = {
+    blue100: '#002b3d',
+    blue90: '#003b54',
+    blue80: '#004c6b',
+    blue70: '#005d82',
+    blue60: '#006e99',
+    blue50: '#f3f4f6', 
+    blue40: '#00688d',
+    blue30: '#33afdd',
+    blue20: '#4db9e1',
+    blue10: '#66c3e5',
+    blue5: '#80cdea',
+    blue0: '#b3e1f2'
+  };
